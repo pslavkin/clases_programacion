61d03847ab42d56c4879c1212e9e04d36de564ea
pablo slavkin <slavkin.pablo@gmail.com>
2022-06-09 21:33:47 -0300

    add snake

---
+++ clases/clases.py
@@ -0,0 +1,22 @@
+
+class miclase():
+    def f1(self):
+        print("soy f1")
+
+    jj=4
+    var4="hola"
+
+kk=4
+
+def main():
+    a = miclase()
+    a.jj=5
+    print(a.jj)
+    
+    b = miclase()
+    b.jj=6
+    print(b.jj)
+
+if __name__=='__main__':
+    main()
+
--- vending_machine/python/v.py
+++ vending_machine/python/v.py
@@ -1,31 +1,28 @@
-lista_prod    = [ 
-#         prod    prec stock
-        ["coca"  ,100,   4] ,
-        ["seven" ,232,   1] ,
-        ["fanta" ,300,   2] ,
-        ["pepsi" ,200,   3] ,
-        ]
+lista_prod	  = [ 
+#		  prod	  prec stock
+		["coca"  ,100,	 4] ,
+		["seven" ,232,	 1] ,
+		["fanta" ,300,	 2] ,
+		["pepsi" ,200,	 3] ,
+		]
 
 def main():
-        producto_deseado = input("ingrese el producto deseado: ")
-        cantidad_deseada = int(input("ingrese la cantidad deseado: "))
+		producto_deseado = input("ingrese el producto deseado: ")
+		cantidad_deseada = int ( input("ingrese la cantidad deseado: "))
 
-        for producto_stock in lista_prod:
-            if(producto_deseado == producto_stock[ 0 ]):
-                print("la",producto_stock[ 0 ],"vale", producto_stock [ 1 ], "total",cantidad_deseada*producto_stock[ 1 ])
-                pago   = int(input("ingrese el pago: "))
-                vuelto = pago - producto_stock[1]*cantidad_deseada
-                print("su vuelto es: ",vuelto , ". Muchas gracias por su compra" )
-                return
-        print("producto no encontrado")
+		for producto_stock in lista_prod:
+			if(producto_deseado == producto_stock[ 0 ]):
+				print("la",producto_stock[ 0 ],"vale", producto_stock [ 1 ], "total",cantidad_deseada*producto_stock[ 1 ])
+				pago   = int(input("ingrese el pago: "))
+				vuelto = pago - producto_stock[1]*cantidad_deseada
+				print("su vuelto es: ",vuelto , ". Muchas gracias por su compra" )
+				return
+		print("producto no encontrado")
 
-        return
+		return
 
 
 if __name__=='__main__':
-
    while True:
-        main()
-
-
+		main()
 
