8897acd7126e62e0ed8174c17813eb41c64ce272
pablo slavkin <slavkin.pablo@gmail.com>
2022-07-14 21:37:00 -0300

    refactor, while/break, for+if in terms of modulo

---
+++ functions_and_vars/functions_and_vars.py
@@ -0,0 +1,17 @@
+
+
+def calcDiaSemana(dia):
+    res = 0
+
+    if dia=='domingo':
+        res=0
+    else:
+        if dia == 'lunes':
+            res=1
+
+    return res 
+
+if __name__ == '__main__':
+
+    d = calcDiaSemana("domingo")
+    print(d)
--- pygame/snake_course/1_snake_game/functions_and_vars.py
+++
@@ -1,8 +0,0 @@
-
-
-def f1(p):
-    return 2*p
-
-if __name__ == '__main__':
-    a=f1(10)
-    print(a)
